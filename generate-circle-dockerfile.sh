167332c5b0f9232c839cd9275019129b0a661cf5
Benjamin Quorning <benjamin@quorning.net>
2018-09-24 09:15:09 +0200

    Rework ENV variables
    The environment variables in the CircleCI `build` job seem less useful now that
    the script has been extracted.
    
    The `TAG` variable in generate-circle-dockerfile.sh on the other hand is
    prominent enough to get its own line :-)

--- generate-circle-dockerfile.sh
+++ generate-circle-dockerfile.sh
@@ -5,7 +5,8 @@ git clone --depth 1 --branch master https://github.com/circleci/circleci-images.
 pushd $repository/ruby
 
 # We only want to build a "latest" tagged image
-TAG="latest" && echo "Tags: ${TAG}" > manifest
+TAG="latest"
+echo "Tags: ${TAG}" > manifest
 
 # Builds a new Docker image with BASE_REPO:TAG as its base image.
 MANIFEST_SOURCE="file://$repository/ruby/manifest" \
@@ -15,4 +16,4 @@ MANIFEST_SOURCE="file://$repository/ruby/manifest" \
 
 popd
 
-cp -v $repository/ruby/images/latest/Dockerfile Dockerfile.circle
+cp -v $repository/ruby/images/${TAG}/Dockerfile Dockerfile.circle
