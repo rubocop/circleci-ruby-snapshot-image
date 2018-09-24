52efd8a8b0c524752231af8ef53acef393d54bac
Benjamin Quorning <benjamin@quorning.net>
2018-09-24 09:12:03 +0200

    Use `latest` tag also for `ruby-snapshot` image
    The `circleci-ruby-snapshot` image is tagget with `latest`, so to make things
    less confusing let's tag the "intermediary" `ruby-snapshot` image with `latest`
    as well, instead of `snapshot`.
    
    To recap, instead of
    
      - bquorning/ruby-snapshot:snapshot
      - bquorning/circleci-ruby-snapshot:latest
    
    we now have
    
      - bquorning/ruby-snapshot:latest
      - bquorning/circleci-ruby-snapshot:latest

--- generate-circle-dockerfile.sh
+++ generate-circle-dockerfile.sh
@@ -4,8 +4,8 @@ repository=`mktemp -d`
 git clone --depth 1 --branch master https://github.com/circleci/circleci-images.git $repository
 pushd $repository/ruby
 
-# We only want to build a "snapshop" tagged image
-TAG="snapshot" && echo "Tags: ${TAG}" > manifest
+# We only want to build a "latest" tagged image
+TAG="latest" && echo "Tags: ${TAG}" > manifest
 
 # Builds a new Docker image with BASE_REPO:TAG as its base image.
 MANIFEST_SOURCE="file://$repository/ruby/manifest" \
@@ -15,4 +15,4 @@ MANIFEST_SOURCE="file://$repository/ruby/manifest" \
 
 popd
 
-cp -v $repository/ruby/images/snapshot/Dockerfile Dockerfile.circle
+cp -v $repository/ruby/images/latest/Dockerfile Dockerfile.circle
