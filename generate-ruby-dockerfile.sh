f01787548f30af49e810c58b1a2bfe47a96e0349
Benjamin Quorning <benjamin@quorning.net>
2019-01-07 09:14:05 +0100

    More restrictire regexes for sed
    When creating Ruby Dockerfile, use more restrictive regular expressions.

--- generate-ruby-dockerfile.sh
+++ generate-ruby-dockerfile.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 curl https://raw.githubusercontent.com/docker-library/ruby/master/2.6/stretch/Dockerfile |
-  sed 's/^ENV.*RUBY_MAJOR .*$//' |
-  sed 's/^ENV.*RUBY_VERSION .*$//' |
-  sed 's/^ENV.*RUBY_DOWNLOAD_SHA256 .*$//' |
-  sed 's/https:\/\/cache.ruby-lang.org\/pub\/ruby\/${RUBY_MAJOR%-rc}\/ruby-$RUBY_VERSION.tar.xz/https:\/\/cache.ruby-lang.org\/pub\/ruby\/snapshot.tar.xz/' |
-  sed 's/&& echo "$RUBY_DOWNLOAD_SHA256 \*ruby.tar.xz" | sha256sum -c - \\/\\/' \
+  sed 's/^ENV RUBY_MAJOR 2\.6$//' |
+  sed 's/^ENV RUBY_VERSION 2\.6\.0$//' |
+  sed 's/^ENV RUBY_DOWNLOAD_SHA256 [0-9a-f]\{64\}$//' |
+  sed 's|https://cache\.ruby-lang\.org/pub/ruby/${RUBY_MAJOR%-rc}/ruby-$RUBY_VERSION\.tar\.xz|https://cache.ruby-lang.org/pub/ruby/snapshot.tar.xz|' |
+  sed 's/&& echo "$RUBY_DOWNLOAD_SHA256 \*ruby\.tar\.xz" | sha256sum -c - \\/\\/' \
   > Dockerfile.ruby
