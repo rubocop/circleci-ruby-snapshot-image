4f867370259263fd75bd831a66bfec339b1fe1d1
Benjamin Quorning <benjamin@quorning.net>
2019-01-31 21:16:29 +0100

    Fix generate-ruby-dockerfile.sh to work for 2.6.1

--- generate-ruby-dockerfile.sh
+++ generate-ruby-dockerfile.sh
@@ -2,7 +2,7 @@
 
 curl https://raw.githubusercontent.com/docker-library/ruby/master/2.6/stretch/Dockerfile |
   sed 's/^ENV RUBY_MAJOR 2\.6$//' |
-  sed 's/^ENV RUBY_VERSION 2\.6\.0$//' |
+  sed 's/^ENV RUBY_VERSION 2\.6\.[0-9][0-9]*$//' |
   sed 's/^ENV RUBY_DOWNLOAD_SHA256 [0-9a-f]\{64\}$//' |
   sed 's|&& wget -O ruby.tar.xz "https://cache.ruby-lang.org/pub/ruby/${RUBY_MAJOR%-rc}/ruby-$RUBY_VERSION.tar.xz" ||' |
   sed 's/&& echo "$RUBY_DOWNLOAD_SHA256 \*ruby.tar.xz" | sha256sum -c - //' |
